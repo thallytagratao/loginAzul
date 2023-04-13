7071ad1d7cb8422c707dfa61cd30c47987440401
Thallyta <thallytaev@gmail.com>
2023-04-13 11:15:27 -0300

    Loader and css changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,37 @@
 function handleSingIn(){
-    document.getElementById('errorUser').style.display = "none"
-    document.getElementById('errorPassword').style.display = "none"  
 
-    const login = document.getElementById('user').value;
-    const password = document.getElementById('password').value;
+    document.getElementById('lds-spinner').style.display = "block"
+    
+    setTimeout(() => {
 
-    if(login == ""){
-    document.getElementById('errorUser').style.display = "block" 
-    return
-    }
-
-    if(password == ""){
-        document.getElementById('errorPassword').style.display = "block" 
+        document.getElementById('lds-spinner').style.display = "none"
+        document.getElementById('errorUser').style.display = "none"
+        document.getElementById('errorPassword').style.display = "none"  
+        
+        const login = document.getElementById('user').value;
+        const password = document.getElementById('password').value;
+    
+        if(login == ""){
+        document.getElementById('errorUser').style.display = "block" 
         return
         }
+    
+        if(password == ""){
+            document.getElementById('errorPassword').style.display = "block" 
+            return
+            }
+    
+        if (login == "admin" && password == "admin"){
+            document.getElementById('user').value = "";
+            document.getElementById('password').value = "";
+            alert('sucesso');
+            
+        } else{
+            document.getElementById('user').value = "";
+            document.getElementById('password').value = "";
+            alert('Usuário e senha não encontrados, favor verificar e tentar novamente.');
+        }
 
-    if (login == "admin" && password == "admin"){
-        document.getElementById('user').value = "";
-        document.getElementById('password').value = "";
-        alert('sucesso');
-        
-    } else{
-        document.getElementById('user').value = "";
-        document.getElementById('password').value = "";
-        alert('Usuário e senha não encontrados, favor verificar e tentar novamente.');
-    }
+    }, 1000);
+    
 }
